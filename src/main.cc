07c565becdbe42b3edff486bc660bdaef152a981
SHEST4 <stanislav.shestakov.02@mail.ru>
2026-09-12 12:13:04 +0300

    Init

---
+++ src/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <simdjson.h>
+
+int main(int argc, const char* argv[]) {
+    
+
+    return (0);
+}
